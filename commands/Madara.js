84587470a1ae36f4615092aa30ce2eeaabce11fc
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-06-14 09:50:52 +0000

    Update Madara.js

--- commands/Madara.js
+++ commands/Madara.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "Sharingan", reaction: "😈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "shan", reaction: "😈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '𝐒𝚮𝚫𝚪𝚰𝚴𝐆𝚫𝚴 𝚫𝐂𝚻𝚰𝛁𝚫𝚻𝚵𝐃 \n\n ' + "║❒ 𝐓𝐘𝐏𝐄 𝐌𝐄𝐍𝐔 𝐓𝐎 𝐒𝐄𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒";
     let d = '                                            ║❒ 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 https://t.me/+H2CcYqSn6zFhZTM0';
