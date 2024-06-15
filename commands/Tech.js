2998bc2c77724aa81ccf3e37ecb45403a867c0bf
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-06-15 14:51:45 +0000

    Update Tech.js

--- commands/Tech.js
+++ commands/Tech.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "Bryant tech", reaction: "🥲", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "developer2", reaction: "🥲", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VacpEdXIt5rqKLB9nC1L";
     let d = '                                     ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@BryantXtech';
